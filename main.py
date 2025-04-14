f1a22377f623ecb7b9232cf3234a68c0c7d6df7f
Thomas Fabri <tsfabri@gmail.com>
2025-04-14 17:26:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     print("função foi chamada")
-    return {"teste": "deu errado"}
+    return {"teste": "deu certo"}
